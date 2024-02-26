0d0bd8342a3c58f9480993753978caca5bab0b73
Daniel Minogue <danielminogue@MacBook-Pro.local>
2024-02-25 23:06:39 -0700

    We setup our stdin by creating two functions in play.js: setupInput and handleUserInput. These help us establish our UI (user interface) where players can send keyboard commands which we will soon use to control the snake.

--- client.js
+++ client.js
@@ -11,6 +11,18 @@ const connect = function () {
       // code that does something when the connection is first established
       console.log("Server Says", data)
     });
+
+    conn.on("connect", () => {
+            conn.write("Successfully connected to game server.");
+            conn.write("Name: BHB");
+
+            // setTimeout(() => {conn.write( "Move: up")}, 1000);
+            // setTimeout(() => {conn.write( "Move: up")}, 1500);
+            // setTimeout(() => {conn.write( "Move: down")}, 1800);
+            // setTimeout(() => {conn.write( "Move: up")}, 2000);
+            
+            
+    });
     
     // interpret incoming data as text
     conn.setEncoding("utf8");
